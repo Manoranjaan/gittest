cc9237711feb5f08bfaa910fb3995bff1cde5a87
manoranjaan <manoranjaan13@gmail.com>
2021-03-26 13:30:06 +0530

    https://github.com/Manoranjaan/gittest.git

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p&kfbevpnjzyke2+&#)hc1u^d^=qqg=j70*mj(7gvq1=g*2m60'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
